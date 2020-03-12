d07bddf01c4f911fbe6f521cf94fddce324f4bb6
vnesterov <48258305+VladNesterov@users.noreply.github.com>
2020-03-12 07:30:53 +0300

    Update ChatServiceImpl.java

--- src/main/java/com/github/vladnesterov/vladchat/ChatServiceImpl.java
+++ src/main/java/com/github/vladnesterov/vladchat/ChatServiceImpl.java
@@ -14,33 +14,45 @@ import java.util.List;
 @Service
 public class ChatServiceImpl implements ChatService {
 
+    private MessageRepository messageRepository;
+
     @Autowired
-    MessageRepository messageRepository;
+    public ChatServiceImpl(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 
     @Override
-    public List<MessageDto> getMessages(String author) {
+    public List<MessageDto> getMessages(String author, String targetUser) {
         List<MessageDto> result = new ArrayList<>();
 
         List<Message> messages = messageRepository.findAll();
-
         for (Message message : messages) {
-            if (author == null || message.getAuthor().equals(author)) {
+
+            boolean authorCorrect = author == null
+                    || author.equals(message.getAuthor());
+            boolean targetUserCorrect = targetUser == null
+                    || targetUser.equals(message.getTargetUser());
+
+            if (authorCorrect && targetUserCorrect) {
                 MessageDto messageDto = new MessageDto();
                 messageDto.setCreatedAt(message.getCreatedAt());
                 messageDto.setMessage(message.getMessage());
                 messageDto.setAuthor(message.getAuthor());
                 result.add(messageDto);
             }
+
         }
+
         return result;
     }
 
     @Override
-    public void sendMessage(final String message, final String author) {
+    public void sendMessage(final String message, final String author,final String targetUser) {
         Message entity = new Message();
         entity.setMessage(message);
         entity.setCreatedAt(LocalDateTime.now());
         entity.setAuthor(author);
+        entity.setTargetUser(targetUser);
         messageRepository.save(entity);
     }
 
