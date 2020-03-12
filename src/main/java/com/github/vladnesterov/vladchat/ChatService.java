4b7fb8f29b1e478c2008adc45cb6bdffab9d9349
vnesterov <48258305+VladNesterov@users.noreply.github.com>
2020-03-12 07:31:42 +0300

    Update ChatService.java
    add target user

--- src/main/java/com/github/vladnesterov/vladchat/ChatService.java
+++ src/main/java/com/github/vladnesterov/vladchat/ChatService.java
@@ -5,7 +5,7 @@ import com.github.vladnesterov.vladchat.dto.MessageDto;
 import java.util.List;
 
 public interface ChatService {
-    List<MessageDto> getMessages(String author);
+    List<MessageDto> getMessages(String author, String targetUser);
 
-    void sendMessage(String message, final String author);
+    void sendMessage(String message, String author, String targetUser);
 }
